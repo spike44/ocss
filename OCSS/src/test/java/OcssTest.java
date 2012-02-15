9df26a475a067158a28b310f50ba0efa48771254
burning29 <burning29@hanmail.net>
2012-02-16 08:49:42 +0900

    setting

--- OCSS/src/test/java/OcssTest.java
+++ OCSS/src/test/java/OcssTest.java
@@ -4,7 +4,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:config/applicationContext.xml","classpath:config/servletContext.xml"})
-public class TwiiterTest {
+public class OcssTest {
 
 
 
